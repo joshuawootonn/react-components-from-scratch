d865626b87e6465e5d67dc0ff4724dd76f732f1a
Josh Wootonn <joshuawootonn@gmail.com>
2023-01-02 09:56:02 -0600

    feat: initial linting, formatting, styling setup

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['Josefin Sans', ...defaultTheme.fontFamily.sans],
+            },
+            colors: {
+                pink: '#FE65B7',
+                blue: '#446DF6',
+                orange: '#FBB02D',
+                yellow: '#FFFF4C',
+                green: '#ACECA1',
+                purple: '#AB87FF',
+                lightest: '#FFFFFF',
+                light: '#EFEFEF',
+                medium: '#CFCFCF',
+                dark: '#8F8F8F',
+                darkest: '#333333',
+            },
+        },
+    },
+    plugins: [],
+}
