bcb670ca83b8e3401058028401e1325693e86265
Josh Wootonn <joshuawootonn@gmail.com>
2023-01-03 08:16:43 -0600

    feat: implement focus boundary entry

--- components/toggle-group.tsx
+++ components/toggle-group.tsx
@@ -17,14 +17,18 @@ import {
 
 const ToggleGroupContext = createContext<{
     selectedId: string | null
+    focusedId: string | null
     select: Dispatch<SetStateAction<string | null>>
+    focus: Dispatch<SetStateAction<string | null>>
     registerNode: (id: string, element: HTMLElement) => void
     deregisterNode: (id: string) => void
     elements: [string, HTMLElement][]
     getToggleItems: () => { id: string; element: HTMLElement }[]
 }>({
     selectedId: null,
+    focusedId: null,
     select: () => {},
+    focus: () => {},
     registerNode: () => {},
     deregisterNode: () => {},
     elements: [],
@@ -37,6 +41,7 @@ type ToggleGroupProps = {
 
 export function ToggleGroup({ children }: ToggleGroupProps) {
     const [selectedId, select] = useState<string | null>(null)
+    const [focusedId, focus] = useState<string | null>(null)
     const elements = useRef<Map<string, HTMLElement>>(new Map())
     const wrapperRef = useRef<HTMLDivElement | null>(null)
 
@@ -49,43 +54,82 @@ export function ToggleGroup({ children }: ToggleGroupProps) {
             .map(([id, element]) => ({ id, element }))
     }, [wrapperRef, elements])
 
-    const value = useMemo(() => {
-        let orderedItems: [string, HTMLElement][] = []
-
-        return {
-            elements: orderedItems,
+    const value = useMemo(
+        () => ({
+            focusedId,
+            focus,
             selectedId,
             select,
             registerNode: function (id: string, element: HTMLElement) {
+                console.log('register', id)
                 elements.current.set(id, element)
             },
             deregisterNode: function (id: string) {
+                console.log('deregister', id)
+
                 elements.current.delete(id)
             },
             getToggleItems,
-        }
-    }, [getToggleItems, selectedId])
+        }),
+        [focusedId, getToggleItems, selectedId],
+    )
 
     return (
         <ToggleGroupContext.Provider value={value}>
-            <div ref={wrapperRef}>{children}</div>
+            <div
+                tabIndex={0}
+                onFocus={e => {
+                    const toggleItems = getToggleItems()
+                    if (toggleItems.length === 0) return
+
+                    const focusIndex = toggleItems.findIndex(item => item.id === focusedId)
+
+                    if (focusIndex !== -1) return toggleItems[focusIndex].element.focus()
+
+                    const selectedIndex = toggleItems.findIndex(item => item.id === selectedId)
+
+                    if (selectedIndex !== -1) return toggleItems[selectedIndex].element.focus()
+
+                    return toggleItems[0].element.focus()
+                }}
+                ref={wrapperRef}
+            >
+                {children}
+            </div>
         </ToggleGroupContext.Provider>
     )
 }
 
-const useToggleContext = function (id: string) {
-    const { selectedId, select, registerNode, deregisterNode, getToggleItems } =
-        useContext(ToggleGroupContext)
+const useToggleContext = function () {
+    const id = useId()
+    const {
+        selectedId,
+        focusedId,
+        select,
+        focus,
+        registerNode,
+        deregisterNode,
+        getToggleItems,
+    } = useContext(ToggleGroupContext)
 
     return {
         ref: (element: HTMLElement | null) =>
             element != null ? registerNode(id, element) : deregisterNode(id),
         isSelected: selectedId === id,
-        tabIndex: selectedId === id ? 0 : -1,
+        tabIndex: focusedId === id ? 0 : -1,
         onClick: (e: MouseEvent) => {
+            e.stopPropagation()
             select(id)
         },
+        onFocus: (e: FocusEvent) => {
+            e.stopPropagation()
+            focus(id)
+        },
+        onBlur: (e: FocusEvent) => {
+            e.stopPropagation()
+        },
         onKeyDown: (e: KeyboardEvent) => {
+            e.stopPropagation()
             const toggleItems = getToggleItems()
             if (toggleItems.length === 0) return
             console.log({ id })
@@ -116,19 +160,20 @@ type ToggleGroupButtonProps = {
 }
 
 export function ToggleGroupButton({ children, className }: ToggleGroupButtonProps) {
-    const id = useId()
-    const { ref, isSelected, onClick, onKeyDown, tabIndex } = useToggleContext(id)
+    const { ref, isSelected, onClick, onFocus, onBlur, onKeyDown, tabIndex } = useToggleContext()
 
     return (
         <button
             data-toggle-group
             ref={ref}
             tabIndex={tabIndex}
+            onFocus={onFocus}
             onKeyDown={onKeyDown}
             onClick={onClick}
+            onBlur={onBlur}
             className={clsx(
                 className,
-                'bg-slate-200 p-1 first:rounded-l last:rounded-r hover:bg-slate-300',
+                'bg-slate-200 p-1 first:rounded-l last:rounded-r hover:bg-slate-300 outline-none border-2 border-transparent focus:border-slate-400',
                 isSelected && 'bg-slate-300',
             )}
         >
