b77a37d63ad9d07805a0e8b7bcdfd87c025acc27
Josh Wootonn <joshuawootonn@gmail.com>
2023-01-08 16:36:40 -0600

    feat: move toggle group to a . pattern

--- components/toggle-group.tsx
+++ components/toggle-group.tsx
@@ -8,34 +8,74 @@ import {
     useState,
     KeyboardEvent,
     MouseEvent,
+    FocusEvent,
     ComponentPropsWithoutRef,
+    useRef,
+    useCallback,
 } from 'react'
 
-import { RovingTabindexRoot, useRovingTabindex } from './roving-tabindex'
+function wrapArray<T>(array: T[], startIndex: number) {
+    return array.map((_, index) => array[(startIndex + index) % array.length])
+}
+
+function focusFirst(candidates: HTMLElement[]) {
+    const previousFocus = document.activeElement
+    while (document.activeElement === previousFocus && candidates.length > 0) {
+        candidates.shift()?.focus()
+    }
+}
+
+type RovingTabindexItem = {
+    value: string
+    element: HTMLElement
+}
 
 const ToggleGroupContext = createContext<{
     value: string | null
     onChange: (value: string) => void
     registerNode: (id: string, element: HTMLElement) => void
     deregisterNode: (id: string) => void
+    currentRovingTabindexValue: string | null
+    onFocus: (id: string) => void
+    onShiftTab: () => void
+    getOrderedItems: () => RovingTabindexItem[]
 }>({
     value: null,
     onChange: () => {},
     registerNode: () => {},
     deregisterNode: () => {},
+    currentRovingTabindexValue: null,
+    onFocus: () => {},
+    onShiftTab: () => {},
+    getOrderedItems: () => [],
 })
 
-type ToggleGroupBaseProps = {
+type RootBaseProps = {
     children: ReactNode | ReactNode[]
     value: string | null
     onChange: (value: string) => void
 }
 
-type ToggleGroupProps = ToggleGroupBaseProps &
-    Omit<ComponentPropsWithoutRef<'div'>, keyof ToggleGroupBaseProps>
+type RootProps = RootBaseProps & Omit<ComponentPropsWithoutRef<'div'>, keyof RootBaseProps>
 
-export function ToggleGroup({ value, onChange, children, ...props }: ToggleGroupProps) {
+function Root({ value, onChange, children, ...props }: RootProps) {
     const [elements] = useState<Map<string, HTMLElement>>(new Map())
+    const [isShiftTabbing, setIsShiftTabbing] = useState(false)
+    const [currentRovingTabindexValue, setCurrentRovingTabindexValue] = useState<string | null>(
+        null,
+    )
+    const rootRef = useRef<HTMLDivElement | null>(null)
+
+    const getOrderedItems = useCallback(() => {
+        if (!rootRef.current || !elements) return []
+        const domElements = Array.from(
+            rootRef.current.querySelectorAll('[data-roving-tabindex-item]'),
+        )
+
+        return Array.from(elements)
+            .sort((a, b) => domElements.indexOf(a[1]) - domElements.indexOf(b[1]))
+            .map(([value, element]) => ({ value, element }))
+    }, [elements])
 
     const providerValue = useMemo(
         () => ({
@@ -47,62 +87,43 @@ export function ToggleGroup({ value, onChange, children, ...props }: ToggleGroup
             deregisterNode: function (id: string) {
                 elements.delete(id)
             },
+            onFocus: function (id: string) {
+                setCurrentRovingTabindexValue(id)
+            },
+            onShiftTab: function () {
+                setIsShiftTabbing(true)
+            },
+            currentRovingTabindexValue,
+            getOrderedItems,
         }),
-        [elements, onChange, value],
+        [currentRovingTabindexValue, elements, getOrderedItems, onChange, value],
     )
 
     return (
         <ToggleGroupContext.Provider value={providerValue}>
-            <RovingTabindexRoot role="group" active={value} elementsById={elements} {...props}>
+            <div
+                role="group"
+                tabIndex={isShiftTabbing ? -1 : 0}
+                onFocus={e => {
+                    if (isShiftTabbing) return
+                    const orderedItems = getOrderedItems()
+                    if (orderedItems.length === 0) return
+
+                    const candidates = [
+                        elements.get(currentRovingTabindexValue ?? ''),
+                        elements.get(value ?? ''),
+                        ...orderedItems.map(i => i.element),
+                    ].filter((element): element is HTMLElement => element != null)
+
+                    focusFirst(candidates)
+                }}
+                onBlur={() => setIsShiftTabbing(false)}
+                ref={rootRef}
+                {...props}
+            >
                 {children}
-            </RovingTabindexRoot>
+            </div>
         </ToggleGroupContext.Provider>
-    )
-}
-
-const useToggleContext = function (value: string) {
-    const { currentRovingTabindexValue, ...rovingProps } = useRovingTabindex(value)
-    const {
-        value: currentValue,
-        onChange,
-        registerNode,
-        deregisterNode,
-    } = useContext(ToggleGroupContext)
-
-    return useMemo(
-        () => ({
-            isSelected: currentValue === value,
-            getToggleProps: (props: ComponentPropsWithoutRef<'button'>) => ({
-                ref: (element: HTMLElement | null) => {
-                    element != null ? registerNode(value, element) : deregisterNode(value)
-                },
-                role: 'radio',
-                ['aria-checked']: currentValue === value,
-                tabIndex: currentRovingTabindexValue === value ? 0 : -1,
-                onClick: (e: MouseEvent<HTMLButtonElement>) => {
-                    e.stopPropagation()
-                    onChange(value)
-                    props.onClick?.(e)
-                },
-                onKeyDown: (e: KeyboardEvent<HTMLButtonElement>) => {
-                    e.stopPropagation()
-                    if (isHotkey('space', e)) {
-                        onChange(value)
-                    }
-                    rovingProps.onKeyDown(e)
-                    props.onKeyDown?.(e)
-                },
-            }),
-        }),
-        [
-            currentRovingTabindexValue,
-            currentValue,
-            deregisterNode,
-            onChange,
-            registerNode,
-            rovingProps,
-            value,
-        ],
     )
 }
 
@@ -115,26 +136,81 @@ type ToggleGroupButtonBaseProps = {
 type ToggleGroupButtonProps = ToggleGroupButtonBaseProps &
     Omit<ComponentPropsWithoutRef<'button'>, keyof ToggleGroupButtonBaseProps>
 
-export function ToggleGroupButton({
-    children,
-    value,
-    className,
-    ...props
-}: ToggleGroupButtonProps) {
-    const { isSelected, getToggleProps } = useToggleContext(value)
+function Button({ children, value, className, ...props }: ToggleGroupButtonProps) {
+    const {
+        value: selectedValue,
+        onChange,
+        registerNode,
+        deregisterNode,
+        currentRovingTabindexValue,
+        onShiftTab,
+        getOrderedItems,
+        onFocus,
+    } = useContext(ToggleGroupContext)
 
     return (
         <button
-            {...getToggleProps({
-                className: clsx(
-                    className,
-                    'bg-slate-200 p-1 first:rounded-l last:rounded-r hover:bg-slate-300 outline-none border-2 border-transparent focus:border-slate-400 transition-all',
-                    isSelected && 'bg-slate-300',
-                ),
-                ...props,
-            })}
+            className={clsx(
+                className,
+                'bg-slate-200 p-1 first:rounded-l last:rounded-r hover:bg-slate-300 outline-none border-2 border-transparent focus:border-slate-400 transition-all',
+                selectedValue === value && 'bg-slate-300',
+            )}
+            ref={(element: HTMLElement | null) => {
+                element != null ? registerNode(value, element) : deregisterNode(value)
+            }}
+            role="radio"
+            aria-checked={selectedValue === value}
+            tabIndex={currentRovingTabindexValue === value ? 0 : -1}
+            data-roving-tabindex-item
+            onClick={(e: MouseEvent<HTMLButtonElement>) => {
+                e.stopPropagation()
+                onChange(value)
+                props.onClick?.(e)
+            }}
+            onMouseDown={(e: MouseEvent) => {
+                onFocus(value)
+            }}
+            onKeyDown={(e: KeyboardEvent<HTMLButtonElement>) => {
+                e.stopPropagation()
+                if (isHotkey('space', e)) {
+                    onChange(value)
+                }
+
+                if (isHotkey('shift+tab', e)) {
+                    onShiftTab()
+                    return
+                }
+
+                const orderedItems = getOrderedItems()
+                if (orderedItems.length === 0) return
+
+                if (isHotkey('down', e) || isHotkey('right', e)) {
+                    const currIndex = orderedItems.findIndex(item => item.value === value)
+                    const candidates = wrapArray(orderedItems, currIndex + 1).map(
+                        item => item.element,
+                    )
+                    focusFirst(candidates)
+                } else if (isHotkey('up', e) || isHotkey('left', e)) {
+                    const currIndex = orderedItems.findIndex(item => item.value === value)
+                    const candidates = wrapArray(orderedItems.reverse(), currIndex + 1).map(
+                        item => item.element,
+                    )
+                    focusFirst(candidates)
+                } else if (isHotkey('home', e)) {
+                    focusFirst(orderedItems.map(item => item.element))
+                } else if (isHotkey('end', e)) {
+                    focusFirst(orderedItems.map(item => item.element).reverse())
+                }
+
+                props.onKeyDown?.(e)
+            }}
+            onFocus={(e: FocusEvent<HTMLButtonElement>) => {
+                e.stopPropagation()
+                onFocus(value)
+            }}
         >
             {children}
         </button>
     )
 }
+export const ToggleGroup = { Root, Button }
--- pages/toggle-group.tsx
+++ pages/toggle-group.tsx
@@ -1,44 +1,48 @@
 import { Bars3BottomLeftIcon, Bars3BottomRightIcon, Bars3Icon } from '@heroicons/react/24/outline'
 import { useState } from 'react'
 
-import { ToggleGroup, ToggleGroupButton } from '../components/toggle-group'
+import { ToggleGroup } from '../components/toggle-group'
 
 export default function ToggleGroupPage() {
     const [textAlignment, setTextAlignment] = useState<string | null>(null)
-    const [currentOption, setOption] = useState<string | null>('two')
+    const [favoriteFruit, changeFavoriteFruit] = useState<string | null>('two')
 
     return (
         <div className="max-w-5xl p-4 lg:p-8 mx-auto prose prose-headings:font-700">
             <h1 className="font-sans">Toggle Group</h1>
 
             <h2>Icon exmaple</h2>
-            <ToggleGroup
+            <ToggleGroup.Root
                 value={textAlignment}
                 onChange={setTextAlignment}
                 aria-label="Text Alignment"
             >
-                <ToggleGroupButton value="left" aria-label="Left Align">
+                <ToggleGroup.Button value="left" aria-label="Left Align">
                     <Bars3BottomLeftIcon className="h-5 w-5" />
-                </ToggleGroupButton>
-                <ToggleGroupButton value="center" aria-label="Center Align">
+                </ToggleGroup.Button>
+                <ToggleGroup.Button value="center" aria-label="Center Align">
                     <Bars3Icon className="h-5 w-5" />
-                </ToggleGroupButton>
-                <ToggleGroupButton value="right" aria-label="Right Align">
+                </ToggleGroup.Button>
+                <ToggleGroup.Button value="right" aria-label="Right Align">
                     <Bars3BottomRightIcon className="h-5 w-5" />
-                </ToggleGroupButton>
-            </ToggleGroup>
-            <h2>Text example w/ default value</h2>
-            <ToggleGroup value={currentOption} onChange={setOption} aria-label="Option selector">
-                <ToggleGroupButton value="one" className="px-3">
-                    Option 1
-                </ToggleGroupButton>
-                <ToggleGroupButton value="two" className="px-3">
-                    Option 2
-                </ToggleGroupButton>
-                <ToggleGroupButton value="three" className="px-3">
-                    Option 3
-                </ToggleGroupButton>
-            </ToggleGroup>
+                </ToggleGroup.Button>
+            </ToggleGroup.Root>
+            <h2>What is your favorite fruit?</h2>
+            <ToggleGroup.Root
+                value={favoriteFruit}
+                onChange={changeFavoriteFruit}
+                aria-label="What is your favorite?"
+            >
+                <ToggleGroup.Button value="one" className="px-2">
+                    Strawberry 🍓
+                </ToggleGroup.Button>
+                <ToggleGroup.Button value="two" className="px-2">
+                    Banana 🍌
+                </ToggleGroup.Button>
+                <ToggleGroup.Button value="three" className="px-2">
+                    Apple 🍏
+                </ToggleGroup.Button>
+            </ToggleGroup.Root>
         </div>
     )
 }
