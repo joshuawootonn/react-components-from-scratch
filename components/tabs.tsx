73eec89c54b68e40d80c88b7662a68fb8a43f965
Josh Wootonn <joshuawootonn@gmail.com>
2023-01-20 06:29:56 -0600

    fix: commented out wip tabs component

--- components/tabs.tsx
+++ components/tabs.tsx
@@ -1,55 +1,56 @@
-import { ComponentPropsWithoutRef, createContext, ReactNode, useMemo, useState } from 'react'
+// this is wip
 
-import { RovingTabindexRoot } from './roving-tabindex'
+export {}
 
-const TabsContext = createContext<{ value?: string; setValue: (value: string) => void }>({
-    value: undefined,
-    setValue: () => {},
-})
+// import { ComponentPropsWithoutRef, createContext, ReactNode, useMemo, useState } from 'react'
 
-type TabsRootProps = {
-    defaultValue: string
-    children?: ReactNode | ReactNode[]
-}
+// import { RovingTabindexRoot } from './roving-tabindex'
 
-function TabsRoot({ defaultValue, children }: TabsRootProps) {?
-    const [elements] = useState<Map<string, HTMLElement>>(new Map())
-    const [value, onChange] = useState<string>(defaultValue)
+// const TabsContext = createContext<{ value?: string; setValue: (value: string) => void }>({
+//     value: undefined,
+//     setValue: () => {},
+// })
 
+// type TabsRootProps = {
+//     defaultValue: string
+//     children?: ReactNode | ReactNode[]
+// }
 
-    const providerValue = useMemo(
-        () => ({
-            value,
-            onChange,
-            registerNode: function (id: string, element: HTMLElement) {
-                elements.set(id, element)
-            },
-            deregisterNode: function (id: string) {
-                elements.delete(id)
-            },
-        }),
-        [elements, onChange, value],
-    )
+// function TabsRoot({ defaultValue, children }: TabsRootProps) {?
+//     const [elements] = useState<Map<string, HTMLElement>>(new Map())
+//     const [value, onChange] = useState<string>(defaultValue)
 
+//     const providerValue = useMemo(
+//         () => ({
+//             value,
+//             onChange,
+//             registerNode: function (id: string, element: HTMLElement) {
+//                 elements.set(id, element)
+//             },
+//             deregisterNode: function (id: string) {
+//                 elements.delete(id)
+//             },
+//         }),
+//         [elements, onChange, value],
+//     )
 
-    return (
-        <TabsContext.Provider value={{ value, setValue }}>
-            <RovingTabindexRoot active={value}>{children}</RovingTabindexRoot>
-        </TabsContext.Provider>
-    )
-}
+//     return (
+//         <TabsContext.Provider value={{ value, setValue }}>
+//             <RovingTabindexRoot active={value}>{children}</RovingTabindexRoot>
+//         </TabsContext.Provider>
+//     )
+// }
 
-type TabsButtonBaseProps = {}
+// type TabsButtonBaseProps = {}
 
-type TabsButtonsProps = TabsButtonBaseProps &
-    Omit<ComponentPropsWithoutRef<'button'>, keyof TabsButtonBaseProps>
+// type TabsButtonsProps = TabsButtonBaseProps &
+//     Omit<ComponentPropsWithoutRef<'button'>, keyof TabsButtonBaseProps>
 
-function TabButton(props: TabsButtonsProps) {
+// function TabButton(props: TabsButtonsProps) {
 
-    
-    return <button {...props}>{props.children}</button>
-}
+//     return <button {...props}>{props.children}</button>
+// }
 
-function TabContent() {
-    return null
-}
+// function TabContent() {
+//     return null
+// }
