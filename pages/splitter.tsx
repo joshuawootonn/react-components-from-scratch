66554d6f22018a3b04d1b86dc1dc08c4b2c104fe
Josh Wootonn <joshuawootonn@gmail.com>
2023-05-28 08:37:10 -0500

    feat: add open / close animation

--- pages/splitter.tsx
+++ pages/splitter.tsx
@@ -1,8 +1,10 @@
 import clsx from 'clsx'
+import { motion } from 'framer-motion'
+import clamp from 'lodash.clamp'
 import { PointerEvent as ReactPointerEvent, useRef, useState } from 'react'
 
 import { TreeviewArrow } from 'components/treeview/article/part-3/animatedTreeview'
-import { nextprojectInitialValues } from 'lib/treeview'
+import { longInitialValues, nextprojectInitialValues } from 'lib/treeview'
 
 export default function SplitterPage() {
     const [selected, select] = useState<string | null>(null)
@@ -14,26 +16,53 @@ export default function SplitterPage() {
 
     return (
         <div className="flex">
-            <div
+            <motion.div
                 className={clsx(
-                    'h-screen max-h-screen overflow-y-auto flex-shrink-0',
+                    'h-screen max-h-screen overflow-y-auto flex-shrink-0 p-2',
                     {
                         ['cursor-col-resize']: isDragging,
                     },
                 )}
-                style={{ width }}
+                animate={{ width: isOpen ? width : 40 }}
+                transition={{ ease: 'easeOut' }}
             >
-                <TreeviewArrow.Root
-                    value={selected}
-                    onChange={select}
-                    className="p-2 not-prose"
-                    label="File Explorer"
+                <button onClick={() => setOpen(isOpen => !isOpen)}>
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        strokeWidth={1.5}
+                        stroke="currentColor"
+                        className="w-6 h-6"
+                    >
+                        <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            d="M12 4.5v15m7.5-7.5h-15"
+                        />
+                    </svg>
+                </button>
+
+                <motion.div
+                    className="bg-gray-200"
+                    initial={false}
+                    animate={{
+                        x: isOpen ? 0 : -width,
+                    }}
+                    transition={{ ease: 'easeOut' }}
                 >
-                    {nextprojectInitialValues.map(node => (
-                        <TreeviewArrow.Node node={node} key={node.id} />
-                    ))}
-                </TreeviewArrow.Root>
-            </div>
+                    <TreeviewArrow.Root
+                        value={selected}
+                        onChange={select}
+                        className={clsx('not-prose')}
+                        label="File Explorer"
+                    >
+                        {longInitialValues.map(node => (
+                            <TreeviewArrow.Node node={node} key={node.id} />
+                        ))}
+                    </TreeviewArrow.Root>
+                </motion.div>
+            </motion.div>
 
             <div
                 onPointerDown={(e: ReactPointerEvent) => {
@@ -44,9 +73,13 @@ export default function SplitterPage() {
 
                     function onPointerMove(e: PointerEvent) {
                         setWidth(
-                            originalWidth.current +
-                                e.clientX -
-                                originalClientX.current,
+                            clamp(
+                                originalWidth.current +
+                                    e.clientX -
+                                    originalClientX.current,
+                                200,
+                                400,
+                            ),
                         )
                     }
 
@@ -63,11 +96,164 @@ export default function SplitterPage() {
                         once: true,
                     })
                 }}
-                className={clsx('w-2 cursor-col-resize shrink-0 shadow', {
-                    ['bg-gray-50']: isDragging,
-                })}
+                className={clsx(
+                    'w-2 cursor-col-resize shrink-0 shadow',
+                    {
+                        ['bg-gray-50']: isDragging,
+                    },
+                    isOpen ? 'w-2' : 'w-2',
+                )}
             />
-            <div className="">main content</div>
+            <div className="">
+                <div>
+                    <div>
+                        <TreeviewArrow.Root
+                            value={selected}
+                            onChange={select}
+                            className={clsx(
+                                'not-prose border',
+                                'relative border-transparent',
+                                // isOpen
+                                //     ? 'relative border-transparent'
+                                //     : 'absolute border-gray-100',
+                            )}
+                            label="File Explorer"
+                        >
+                            {longInitialValues.map(node => (
+                                <TreeviewArrow.Node node={node} key={node.id} />
+                            ))}
+                        </TreeviewArrow.Root>
+                        <TreeviewArrow.Root
+                            value={selected}
+                            onChange={select}
+                            className={clsx(
+                                'not-prose border',
+                                'relative border-transparent',
+                                // isOpen
+                                //     ? 'relative border-transparent'
+                                //     : 'absolute border-gray-100',
+                            )}
+                            label="File Explorer"
+                        >
+                            {longInitialValues.map(node => (
+                                <TreeviewArrow.Node node={node} key={node.id} />
+                            ))}
+                        </TreeviewArrow.Root>
+                        <TreeviewArrow.Root
+                            value={selected}
+                            onChange={select}
+                            className={clsx(
+                                'not-prose border',
+                                'relative border-transparent',
+                                // isOpen
+                                //     ? 'relative border-transparent'
+                                //     : 'absolute border-gray-100',
+                            )}
+                            label="File Explorer"
+                        >
+                            {longInitialValues.map(node => (
+                                <TreeviewArrow.Node node={node} key={node.id} />
+                            ))}
+                        </TreeviewArrow.Root>
+                        <TreeviewArrow.Root
+                            value={selected}
+                            onChange={select}
+                            className={clsx(
+                                'not-prose border',
+                                'relative border-transparent',
+                                // isOpen
+                                //     ? 'relative border-transparent'
+                                //     : 'absolute border-gray-100',
+                            )}
+                            label="File Explorer"
+                        >
+                            {longInitialValues.map(node => (
+                                <TreeviewArrow.Node node={node} key={node.id} />
+                            ))}
+                        </TreeviewArrow.Root>
+                        <TreeviewArrow.Root
+                            value={selected}
+                            onChange={select}
+                            className={clsx(
+                                'not-prose border',
+                                'relative border-transparent',
+                                // isOpen
+                                //     ? 'relative border-transparent'
+                                //     : 'absolute border-gray-100',
+                            )}
+                            label="File Explorer"
+                        >
+                            {longInitialValues.map(node => (
+                                <TreeviewArrow.Node node={node} key={node.id} />
+                            ))}
+                        </TreeviewArrow.Root>
+                        <TreeviewArrow.Root
+                            value={selected}
+                            onChange={select}
+                            className={clsx(
+                                'not-prose border',
+                                'relative border-transparent',
+                                // isOpen
+                                //     ? 'relative border-transparent'
+                                //     : 'absolute border-gray-100',
+                            )}
+                            label="File Explorer"
+                        >
+                            {longInitialValues.map(node => (
+                                <TreeviewArrow.Node node={node} key={node.id} />
+                            ))}
+                        </TreeviewArrow.Root>
+                        <TreeviewArrow.Root
+                            value={selected}
+                            onChange={select}
+                            className={clsx(
+                                'not-prose border',
+                                'relative border-transparent',
+                                // isOpen
+                                //     ? 'relative border-transparent'
+                                //     : 'absolute border-gray-100',
+                            )}
+                            label="File Explorer"
+                        >
+                            {longInitialValues.map(node => (
+                                <TreeviewArrow.Node node={node} key={node.id} />
+                            ))}
+                        </TreeviewArrow.Root>
+                        <TreeviewArrow.Root
+                            value={selected}
+                            onChange={select}
+                            className={clsx(
+                                'not-prose border',
+                                'relative border-transparent',
+                                // isOpen
+                                //     ? 'relative border-transparent'
+                                //     : 'absolute border-gray-100',
+                            )}
+                            label="File Explorer"
+                        >
+                            {longInitialValues.map(node => (
+                                <TreeviewArrow.Node node={node} key={node.id} />
+                            ))}
+                        </TreeviewArrow.Root>
+                        <TreeviewArrow.Root
+                            value={selected}
+                            onChange={select}
+                            className={clsx(
+                                'not-prose border',
+                                'relative border-transparent',
+                                // isOpen
+                                //     ? 'relative border-transparent'
+                                //     : 'absolute border-gray-100',
+                            )}
+                            label="File Explorer"
+                        >
+                            {longInitialValues.map(node => (
+                                <TreeviewArrow.Node node={node} key={node.id} />
+                            ))}
+                        </TreeviewArrow.Root>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
