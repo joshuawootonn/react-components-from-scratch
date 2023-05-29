ed305fa53ab5e673ca8988877e4bfff335b4943b
Josh Wootonn <joshuawootonn@gmail.com>
2023-05-29 13:28:31 -0500

    fix: numerous problems with splitter

--- pages/splitter.tsx
+++ pages/splitter.tsx
@@ -12,39 +12,55 @@ export default function SplitterPage() {
     const originalWidth = useRef(width)
     const originalClientX = useRef(width)
     const [isDragging, setDragging] = useState(false)
-    const [isOpen, setOpen] = useState(false)
+    const [isOpen, setOpen] = useState(true)
 
     return (
         <div className="flex">
             <motion.div
                 className={clsx(
-                    'h-screen max-h-screen overflow-y-auto flex-shrink-0 p-2',
+                    'flex flex-col space-y-2 relative h-screen max-h-screen flex-shrink-0 py-2 transition-shadow divide-y-2',
                     {
                         ['cursor-col-resize']: isDragging,
                     },
+                    isDragging
+                        ? 'shadow-[rgba(0,0,0,0.2)_-2px_0px_0px_0px_inset]'
+                        : 'shadow-[rgba(0,0,0,0.04)_-2px_0px_0px_0px_inset]',
                 )}
+                initial={false}
                 animate={{ width: isOpen ? width : 40 }}
                 transition={{ ease: 'easeOut' }}
             >
-                <button onClick={() => setOpen(isOpen => !isOpen)}>
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        strokeWidth={1.5}
-                        stroke="currentColor"
-                        className="w-6 h-6"
+                <motion.div
+                    initial={false}
+                    animate={{ x: isOpen ? 0 : -width + 40 }}
+                    transition={{ ease: 'easeOut' }}
+                    className="flex justify-between items-center p-2"
+                >
+                    <div>Non scrolling header</div>
+
+                    <button
+                        className="self-end"
+                        onClick={() => setOpen(isOpen => !isOpen)}
                     >
-                        <path
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            d="M12 4.5v15m7.5-7.5h-15"
-                        />
-                    </svg>
-                </button>
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            strokeWidth={1.5}
+                            stroke="currentColor"
+                            className="w-6 h-6"
+                        >
+                            <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                d="M12 4.5v15m7.5-7.5h-15"
+                            />
+                        </svg>
+                    </button>
+                </motion.div>
 
                 <motion.div
-                    className="bg-gray-200"
+                    className="max-h-96"
                     initial={false}
                     animate={{
                         x: isOpen ? 0 : -width,
@@ -54,7 +70,7 @@ export default function SplitterPage() {
                     <TreeviewArrow.Root
                         value={selected}
                         onChange={select}
-                        className={clsx('not-prose')}
+                        className={clsx('h-full not-prose')}
                         label="File Explorer"
                     >
                         {longInitialValues.map(node => (
@@ -62,48 +78,57 @@ export default function SplitterPage() {
                         ))}
                     </TreeviewArrow.Root>
                 </motion.div>
+
+                <div className="absolute z-10 right-0 w-0 flex-grow-0 top-0 bottom-0">
+                    <div
+                        onPointerDown={(e: ReactPointerEvent) => {
+                            // this prevents dragging from selecting
+                            e.preventDefault()
+
+                            const { ownerDocument } = e.currentTarget
+                            originalWidth.current = width
+                            originalClientX.current = e.clientX
+                            setDragging(true)
+
+                            function onPointerMove(e: PointerEvent) {
+                                setWidth(
+                                    clamp(
+                                        originalWidth.current +
+                                            e.clientX -
+                                            originalClientX.current,
+                                        200,
+                                        400,
+                                    ),
+                                )
+                            }
+
+                            function onPointerUp() {
+                                ownerDocument.removeEventListener(
+                                    'pointermove',
+                                    onPointerMove,
+                                )
+                                setDragging(false)
+                            }
+
+                            ownerDocument.addEventListener(
+                                'pointermove',
+                                onPointerMove,
+                            )
+                            ownerDocument.addEventListener(
+                                'pointerup',
+                                onPointerUp,
+                                {
+                                    once: true,
+                                },
+                            )
+                        }}
+                        className={clsx(
+                            'w-3 h-full cursor-col-resize shrink-0',
+                        )}
+                    />
+                </div>
             </motion.div>
 
-            <div
-                onPointerDown={(e: ReactPointerEvent) => {
-                    const { ownerDocument } = e.currentTarget
-                    originalWidth.current = width
-                    originalClientX.current = e.clientX
-                    setDragging(true)
-
-                    function onPointerMove(e: PointerEvent) {
-                        setWidth(
-                            clamp(
-                                originalWidth.current +
-                                    e.clientX -
-                                    originalClientX.current,
-                                200,
-                                400,
-                            ),
-                        )
-                    }
-
-                    function onPointerUp() {
-                        ownerDocument.removeEventListener(
-                            'pointermove',
-                            onPointerMove,
-                        )
-                        setDragging(false)
-                    }
-
-                    ownerDocument.addEventListener('pointermove', onPointerMove)
-                    ownerDocument.addEventListener('pointerup', onPointerUp, {
-                        once: true,
-                    })
-                }}
-                className={clsx(
-                    'w-2 cursor-col-resize shrink-0 shadow',
-                    {
-                        ['bg-gray-50']: isDragging,
-                    },
-                    isOpen ? 'w-2' : 'w-2',
-                )}
-            />
             <div className="">
                 <div>
                     <div>
