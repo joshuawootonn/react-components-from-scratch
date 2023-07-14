72ce6c3cc487e40abf1cb015eeee38a150200cef
Josh Wootonn <joshuawootonn@gmail.com>
2023-07-14 13:10:47 -0500

    feat: update IS_Safari for mobile safari

--- components/sidebar/IsSafari.ts
+++ components/sidebar/IsSafari.ts
@@ -1,6 +1,10 @@
 const IS_SAFARI =
     typeof navigator !== 'undefined' &&
-    /Safari/.test(navigator.userAgent) &&
-    /Apple Computer/.test(navigator.vendor)
+    ((/Safari/.test(navigator.userAgent) &&
+        /Apple Computer/.test(navigator.vendor)) ||
+        ((!!navigator.userAgent.match(/iPad/i) ||
+            !!navigator.userAgent.match(/iPhone/i)) &&
+            !!navigator.userAgent.match(/WebKit/i) &&
+            !navigator.userAgent.match(/CriOS/i)))
 
 export default IS_SAFARI
